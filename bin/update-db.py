9b6586c357e6f001be5da47f759463cece127f0e
Maurice Faber <maurice@instrukt.ai>
2025-10-01 18:18:09 +0200

    chore(clean): db pruned from old and non relevant sources

--- bin/update-db.py
+++ bin/update-db.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from api.store import get_data
 
 if __name__ == "__main__":
